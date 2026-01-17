3ffa80245c23506207d7c7f1eaf56d399ae72cee
kksum694-eng <kksum694@gmail.com>
2026-01-17 19:48:03 +0530

    Update pc.php

--- pc.php
+++ pc.php
@@ -3,7 +3,7 @@
 const BOT_TOKEN     = '7998704927:AAHoLSwfnN4023NZHhfx29a3eJfXRs6SHRc';
 const ADMIN_CHAT_ID = '';
 const WEBHOOK_URL   = '';
-const CHANNEL_IDS   = ['@sklootmachao1'];
+const CHANNEL_IDS   = [];
 
 function httpCallAdvanced($url, $data = null, $headers = [], $method = "GET", $returnHeaders = false) {
     $ch = curl_init();
